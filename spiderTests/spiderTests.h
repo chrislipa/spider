ed88b1813cca886c6c7fbe4ca08c7fcc0d9f2be0
Chris <Chris@lipa>
2012-07-21 16:14:09 -0700

    aaaa

---
+++ spiderTests/spiderTests.h
@@ -0,0 +1,13 @@
+//
+//  spiderTests.h
+//  spiderTests
+//
+//  Created by Christopher Lipa on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface spiderTests : SenTestCase
+
+@end
---
+++ spiderTests/spiderTests.m
@@ -0,0 +1,32 @@
+//
+//  spiderTests.m
+//  spiderTests
+//
+//  Created by Christopher Lipa on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "spiderTests.h"
+
+@implementation spiderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in spiderTests");
+}
+
+@end
