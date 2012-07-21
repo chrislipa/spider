15e350c6740218399b1572fddd6ea512bec4e831
Chris <Chris@lipa>
2012-07-21 16:12:02 -0700

    aaa

---
+++ spider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  spider
+//
+//  Created by Christopher Lipa on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ spider/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  spider
+//
+//  Created by Christopher Lipa on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
